e0833c576f1bb6554f23c3edea9ee92e20673a2d
Thijmen Maus <thijmen@maus.nu>
2022-12-10 09:24:32 +0100

    feature(layout): make jumbotron content responsive

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -1,9 +1,12 @@
 export function Jumbotron() {
     return (
         <div className="min-h-[300px] h-[25vw] max-h-[650px] w-full relative flex justify-center items-center flex-col gap-8">
-            <h1 className="z-20 w-full text-7xl text-center text-white font-[Kanit] font-bold">Arqel B.V.</h1>
-            <h2 className="z-20 w-full text-center text-2xl font-light text-white opacity-80">Holding Company
-                Since 2022</h2>
+            <h1 className="z-20 w-full text-5xl sm:text-7xl text-center text-white font-[Kanit] font-bold">
+                Arqel B.V.
+            </h1>
+            <h2 className="z-20 w-full text-center text-xl sm:text-2xl font-light text-white opacity-80">
+                Holding Company Since 2022
+            </h2>
             <div className="absolute z-10 h-full w-full bg-gradient-to-b opacity-75 mix-blend-overlay from-secondary to-primary"/>
             <img
                 src="/assets/arnhem.jpeg"
